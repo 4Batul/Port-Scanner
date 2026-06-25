b58613f3c2d0bac96b67337c803647c85288553d
Arhama B <198487498+4Batul@users.noreply.github.com>
2026-06-25 16:23:39 +0500

    Enhance port scanner with CSV support and input options
    Added functionality to read port descriptions from a CSV file and allow custom port input. Enhanced user interaction for scanning options.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import socket
 import sys  
 from concurrent.futures import ThreadPoolExecutor
+import csv
+
+port_dict = {}
+with open('ports.csv', mode='r', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader) 
+    for row in reader:
+        port = int(row[1])
+        description = row[2]
+        port_dict[port] = description
 
 bannerTxt = """                                
   _____ ____ ____    ____            _     ____                                  
@@ -16,8 +26,29 @@ print("Multi-Threaded TCP Port Scanner")
 ports_open = 0
 stack = []  
 target = input("Enter the target (e.g., 127.0.0.1 or scanme.nmap.org): ")
-port_start = int(input("Starting port: "))
-port_end = int(input("Ending Port: "))
+choice = int(input("1.For a range of ports eg, 130-2000 \n2. For custom multiple inputs eg,23,45,135,139\n"))
+if choice == 1:
+    port_start = int(input("Starting port: "))
+    port_end = int(input("Ending Port: "))
+    ports_to_scan = range(port_start, port_end + 1)
+
+else:
+    def get_port_input(prompt):
+        max_ports = 100
+        while True:
+            try:
+                ports_input = input(prompt)
+            # Expecting comma-separated ports, e.g., 80,443,8080
+                ports_list = [int(p.strip()) for p in ports_input.split(',')]
+                if len(ports_list) > max_ports:
+                    print(f"Please enter no more than {max_ports} ports.")
+                else:
+                    return ports_list
+            except ValueError:
+                print("Invalid input. Please enter comma-separated port numbers.")
+    ports_to_scan = get_port_input("Enter ports (comma-separated, e.g., 80,443,8080): ")
+
+
 MAX_THREADS = 100 
 
 def scan_port(port):
@@ -50,7 +81,6 @@ open_ports = []
 try:
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
         # map() passes each port from our range into the scan_port function
-        ports_to_scan = range(port_start, port_end + 1)
         results = executor.map(scan_port, ports_to_scan)
         for res in results:
             if res:
@@ -61,6 +91,6 @@ except KeyboardInterrupt:
 
 print("\n--- Scan Summary ---")
 print(f"Total open ports found: {len(open_ports)}")
-
-
-
+for port, banner in open_ports:
+    description = port_dict.get(port, "No description available")
+    print(f"Port {port}: {description}")
